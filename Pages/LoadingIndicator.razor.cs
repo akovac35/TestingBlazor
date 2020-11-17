29119864ee833313ed2550a2fc90d07d2912530a
Aleksander Kovač <aleksander.kovac@gmail.com>
2020-11-17 19:21:48 +0100

    Updated loading indicator code.

--- Pages/LoadingIndicator.razor.cs
+++ Pages/LoadingIndicator.razor.cs
@@ -14,6 +14,10 @@ namespace TestingBlazor.Pages
 
         protected string Activity1Status { get; set; }
 
+        protected string Activity2Status { get; set; }
+
+        protected string Activity3Status { get; set; }
+
         protected int OnAfterRenderCounter { get; set; }
 
         protected override void OnInitialized()
@@ -52,13 +56,27 @@ namespace TestingBlazor.Pages
             OnAfterRenderCounter++;
         }        
 
-        protected async Task ExecuteActivity1()
+        protected async Task ExecuteActivity1Async()
         {
             Activity1Status = "Executing activity 1 ...";
 
             await Task.Delay(2000);
 
             Activity1Status = "Activity 1 executed.";
+        }
+
+        protected Task ExecuteActivity2()
+        {
+            Activity2Status = "Activity 2 executed.";
+
+            return Task.CompletedTask;
+        }
+
+        protected Task ExecuteActivity3()
+        {
+            Activity3Status = "Activity 3 executing. The page will make another render once the task completes.";
+
+            return Task.Delay(2000);
         }
     }
 }
